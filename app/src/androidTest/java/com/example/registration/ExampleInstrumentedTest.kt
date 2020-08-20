8eeefa7de9e46542929fb96815ae697b11593864
reddy-27 <udayr@xanthus.in>
2020-08-20 14:22:37 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/example/registration/ExampleInstrumentedTest.kt
@@ -0,0 +1,104 @@
+package com.example.registration
+
+import android.content.Context
+import android.service.autofill.Validators.not
+import android.view.View
+import android.widget.Toast
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matcher
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.regex.Pattern.matches
+import javax.xml.validation.Validator
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.registration", appContext.packageName)
+
+        //return appContext
+    }
+
+    @Test
+    fun user_can_enter_first_name() {
+        onView(withId(R.id.first_name)).perform(typeText("uday"))
+    }
+
+    @Test
+    fun user_can_enter_last_name() {
+        onView(withId(R.id.last_name)).perform(typeText("reddy"))
+    }
+
+
+    @Test
+    fun user_can_enter_first_name_last_name_matches() {
+        onView(withId(R.id.first_name)).perform(typeText("raj"))
+        onView(withId(R.id.last_name)).perform(typeText("king"))
+        onView(withId(R.id.button)).perform(click())
+       // onView(withId(R.id.button)).perform(click()).check(matches(not(isEnabled())))
+//        onView(withId(R.id.button)).perform(isEnabled())
+
+
+                    onView(withId(R.id.message)).check(matches("raj(.*)", "raj king"));
+
+
+//            onView(withId(R.id.message))
+//                .perform(click())
+//           .check(matches(withText("welcome raju king")))
+    }
+
+
+    @Test
+    fun first_message() {
+        onView(withId(R.id.first_name)).perform(typeText("abhijeet"))
+        onView(withId(R.id.last_name)).perform(typeText("said"))
+        onView(withId(R.id.button)).perform(click())
+        onView(withId(R.id.message)).check(matches("s", "abhijeet said"));
+
+
+
+    }
+
+
+    /*private fun Any.check(matches: Boolean) {
+        if(matches)
+            Toast.makeText(useAppContext(),"Matched",Toast.LENGTH_LONG).show()
+        else
+            Toast.makeText(useAppContext(),"Not Matched",Toast.LENGTH_LONG).show()
+
+
+        return matches
+    }*/
+}
+
+private fun Any.check(matches: Boolean) {
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/registration/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.registration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        if(BuildConfig.DEBUG){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+        button.setOnClickListener(View.OnClickListener {
+            message.text="welcome ${first_name.text}${last_name.text}"
+        })
+    }
+}
